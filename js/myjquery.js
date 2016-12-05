e3fa7c1e6ed09782e9632cee3bcb678a8d8d74c8
NikitaCanuta <nikitahuaman@gmail.com>
2016-12-05 09:49:09 -0500

    Avance de servicio y nosotros

--- js/myjquery.js
+++ js/myjquery.js
@@ -2,3 +2,7 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
   $('#menu').pushpin({ bottom: $('.container').offset().top });
   });
+
+  $(document).ready(function(){
+      $('.materialboxed').materialbox();
+    });
